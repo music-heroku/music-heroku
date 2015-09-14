8b333ab4a94874cace9252c7da01e62ba127f0f7
music-heroku <music.heroku@gmail.com>
2015-09-13 23:39:25 -0600

    Add artist and title

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
+    title = ""
+    artist = ""
+    return "♫"+artist+"-"+title
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
